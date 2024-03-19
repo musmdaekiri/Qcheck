427dca99a9927f3306874bb01a1d24e0bed3a6c5
musmdaekiri <ghkdldj13@gmail.com>
2024-03-19 09:29:42 +0900

    프로젝트 등록

---
+++ src/main/java/com/example/qcheck/QcheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.qcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QcheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qcheck/QcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
